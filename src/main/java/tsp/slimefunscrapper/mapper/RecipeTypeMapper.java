1c96c90b4c6ff92ddbb79f44afad8c3721fd1241
Silent <perovskiboki@mail.com>
2022-05-21 23:38:31 +0200

    recipe-type names (1.2.0)

--- src/main/java/tsp/slimefunscrapper/mapper/RecipeTypeMapper.java
+++ src/main/java/tsp/slimefunscrapper/mapper/RecipeTypeMapper.java
@@ -7,6 +7,8 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 
 public class RecipeTypeMapper extends JsonMapper<RecipeType> {
@@ -20,8 +22,30 @@ public class RecipeTypeMapper extends JsonMapper<RecipeType> {
     public JsonElement serialize(RecipeType src, Type typeOfSrc, JsonSerializationContext context) {
         JsonObject main = new JsonObject();
         main.addProperty("key", src.getKey().toString());
+        main.addProperty("name", fromRecipeType(src));
         //main.add("machine", context.serialize(src.getMachine(), SlimefunItem.class));
         return main;
+    }
+
+    /**
+     * Attempt to retrieve the name of the {@link RecipeType} constant using reflection.
+     *
+     * @param type The type to check
+     * @return The name, else null.
+     */
+    private String fromRecipeType(RecipeType type) {
+        for (Field field : RecipeType.class.getDeclaredFields()) {
+            int mod = field.getModifiers();
+            if (Modifier.isPublic(mod) && Modifier.isStatic(mod) && Modifier.isFinal(mod)) {
+                try {
+                    if (field.get(null).equals(type)) {
+                        return field.getName();
+                    }
+                } catch (IllegalAccessException ignored) {}
+            }
+        }
+
+        return null;
     }
 
 }
