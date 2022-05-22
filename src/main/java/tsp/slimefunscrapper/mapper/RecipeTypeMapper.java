e25787f2aeffe1c63ece083adb8412a4f1247d8b
Silent <perovskiboki@mail.com>
2022-05-22 15:59:41 +0200

    fix resolving of recipe types

--- src/main/java/tsp/slimefunscrapper/mapper/RecipeTypeMapper.java
+++ src/main/java/tsp/slimefunscrapper/mapper/RecipeTypeMapper.java
@@ -10,6 +10,7 @@ import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
+import java.util.regex.Pattern;
 
 public class RecipeTypeMapper extends JsonMapper<RecipeType> {
 
@@ -34,18 +35,39 @@ public class RecipeTypeMapper extends JsonMapper<RecipeType> {
      * @return The name, else null.
      */
     private String fromRecipeType(RecipeType type) {
+        // Try to resolve constant name
         for (Field field : RecipeType.class.getDeclaredFields()) {
             int mod = field.getModifiers();
             if (Modifier.isPublic(mod) && Modifier.isStatic(mod) && Modifier.isFinal(mod)) {
                 try {
                     if (field.get(null).equals(type)) {
-                        return field.getName();
+                        return capitalizeWords(field.getName());
                     }
                 } catch (IllegalAccessException ignored) {}
             }
         }
 
-        return null;
+        // Some recipe types may not be a constant, this is just a last resort
+        return capitalizeWords(type.getKey().getKey());
+    }
+
+    private final Pattern DELIMITER = Pattern.compile("_");
+
+    private String capitalizeWords(String string) {
+        StringBuilder builder = new StringBuilder();
+        String[] words = DELIMITER.split(string);
+        for (int i = 0; i < words.length; i++) {
+            builder.append(capitalize(words[i]));
+            if (i < words.length - 1) {
+                builder.append(" ");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private String capitalize(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
     }
 
 }
